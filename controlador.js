7485f465f60e88301ba2dad551af56c1f5f294c8
evisborja <evisborja0522@gmail.com>
2021-07-21 16:53:15 -0500

    Initial commit

---
+++ controlador.js
@@ -0,0 +1,5 @@
+//programa que nos ayude a establecer el costo total de una consola de video juegos
+//en el exterior, pago en dolares 
+//pagar por casillero en dolares, costo dependiendo cant. productos
+//pagar envio, depende del peso
+// impuesto de entrada al pais, lo digita aduanas
